6a7e23ea45466c45214a15267f03b18600383d6f
satou-0113 <satou-0113@localhost>
2024-03-12 19:19:56 +0900

    Hello World

---
+++ HelloWorld/src/helloworld/HelloWorld.java
@@ -0,0 +1,12 @@
+package helloworld;
+/**
+* Hello World クラス
+*/
+public class HelloWorld {
+/**
+* main メソッド
+* @param args 実行引数
+*/
+public static void main(String[] args) {
+System.out.println("Hello World!"); }
+}
