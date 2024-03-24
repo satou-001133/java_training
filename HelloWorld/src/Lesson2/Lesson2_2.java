55e95f0774769cda8d0eb60a6537dd90aced6ada
satou-0113 <satou-0113@localhost>
2024-03-24 16:40:21 +0900

    Lesson2

--- HelloWorld/src/Lesson2/Lesson2_2.java
+++ HelloWorld/src/Lesson2/Lesson2_2.java
@@ -1,8 +1,1 @@
-packge Lesson2;
-class Lesson2_1 {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String str = br.readLine(); // ユーザーからの入力を読み取る
-        System.out.println("入力された文字列: " + str);
-    }
-}
+package Lesson2;
